0ffec431b828ecc3d194938f34ca00d098ea2e0e
Sam Pritchard <sampritchard@msn.com>
2017-05-05 13:53:34 +0100

    4 test cases passed

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,15 @@
+def fizzbuzz(number)
+    if
+      number == 3
+      'fizz'
+    elsif
+      number == 5
+      "buzz"
+    elsif
+      number == 10
+      "buzz"
+    else
+      number == 4
+      "4"
+    end
+end
---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,21 @@
+require "fizzbuzz"
+
+describe "fizzbuzz" do
+
+  it "returns 'fizz' when passed 3" do
+    expect(fizzbuzz(3)).to eq "fizz"
+  end
+  it "returns 'buzz' when passed 5" do
+      expect(fizzbuzz(5)).to eq "buzz"
+  end
+
+  it "returns 'buzz' when pass 10" do
+    expect(fizzbuzz(10)).to eq "buzz"
+  end
+
+  it "returns '4' when passed 4" do
+    expect(fizzbuzz(4)).to eq "4"
+  end
+
+
+end
